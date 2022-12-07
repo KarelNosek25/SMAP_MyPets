1cf9982863bf02f0954d17dfe2698cb433a05d10
KarelNosek25 <81867609+KarelNosek25@users.noreply.github.com>
2022-12-07 14:31:56 +0100

    Saving pictures version #1

--- app/src/main/java/com/example/smap_mypets/fragment/Camera.java
+++ app/src/main/java/com/example/smap_mypets/fragment/Camera.java
@@ -1,11 +1,15 @@
 package com.example.smap_mypets.fragment;
 
+import static android.content.ContentValues.TAG;
 import static org.opencv.core.CvType.CV_8UC1;
 import static org.opencv.imgproc.Imgproc.Canny;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,16 +20,25 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Camera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     CameraBridgeViewBase cameraBridgeViewBase;
     BaseLoaderCallback baseLoaderCallback;
     private boolean normal, edge, small, medium, big, blurYes, blurNo, color, blackWhite;
+
+    private ImageView btnCapture;
+    private int ambilGamba = 0;
+    private Mat mRgba;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +78,15 @@ public class Camera extends AppCompatActivity implements CameraBridgeViewBase.Cv
 
         color = intent.getBooleanExtra("colorStatus", false);
         blackWhite = intent.getBooleanExtra("blackWhiteStatus", true);
+
+        btnCapture = findViewById(R.id.capBtn);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Foto", Toast.LENGTH_SHORT).show();
+                ambilGamba = 1;
+            }
+        });
     }
 
     @Override
@@ -79,38 +101,50 @@ public class Camera extends AppCompatActivity implements CameraBridgeViewBase.Cv
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat rgba = inputFrame.rgba();
-        Mat edges = new Mat(rgba.size(), CV_8UC1);
+        mRgba = inputFrame.rgba();
 
         //barevnost obrazu
         if (blackWhite) {
-            Imgproc.cvtColor(rgba, edges, Imgproc.COLOR_RGB2GRAY, 4);
+            Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGB2GRAY, 4);
         }
 
         //rozmazání
         if (blurYes) {
-            Imgproc.blur(rgba, rgba, new Size(5, 5));
+            Imgproc.blur(mRgba, mRgba, new Size(5, 5));
         }
 
         //druh a citlivost kamery
         if (edge && small) {
-            Canny(rgba, edges, 200, 200);
-            return edges;
+            Canny(mRgba, mRgba, 200, 200);
         } else if (edge && medium) {
-            Canny(rgba, edges, 100, 100);
-            return edges;
+            Canny(mRgba, mRgba, 100, 100);
         } else if (edge && big) {
-            Canny(rgba, edges, 50, 50);
-            return edges;
-        } else {
-            Mat frame = inputFrame.rgba();
-            //při černobílém obrazu potřebuji vrátit "edges"
-            if (blackWhite) {
-                return edges;
-            } else {
-                return frame;
-            }
+            Canny(mRgba, mRgba, 50, 50);
         }
+        ambilGamba = fungsiCaptureGambar(ambilGamba, mRgba);
+        return mRgba;
+    }
+
+    private int fungsiCaptureGambar(int ambilGambar, Mat mRgba) {
+        if (ambilGambar == 1) {
+
+            boolean sukses = true;
+
+            Mat save_mat = new Mat();
+            Core.flip(mRgba.t(), save_mat, 1);
+
+            if(color){
+                Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGBA2BGRA);
+            }
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+            String waktuSekarang = sdf.format(new Date());
+            String fileName = getExternalFilesDir(Environment.DIRECTORY_DCIM) + "/" + waktuSekarang + ".jpg";
+
+            Boolean cekSave = Imgcodecs.imwrite(fileName, mRgba);
+            ambilGambar = 0;
+        }
+        return ambilGambar;
     }
 
     @Override
